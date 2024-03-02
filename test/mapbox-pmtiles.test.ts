30e824e5f6556d1b5b830058978fdb3a113f93cc
Majid Hojati <mhojati@uwaterloo.ca>
2024-03-02 00:43:46 -0500

    Update mapbox-pmtiles.test.ts

---
+++ test/mapbox-pmtiles.test.ts
@@ -0,0 +1,10 @@
+// sum.test.js
+import { expect, test } from 'vitest'
+import { PmTilesSource } from '../src/index'
+
+
+const OSM_PMTILE="https://r2-public.protomaps.com/protomaps-sample-datasets/protomaps-basemap-opensource-20230408.pmtiles"
+test('Get pmtiles metadata', async () => {
+    const metadata = await PmTilesSource.getMetadata(OSM_PMTILE)
+    expect(metadata.name).toBe("Basemap")
+})
