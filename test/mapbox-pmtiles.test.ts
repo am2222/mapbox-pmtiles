7422b7da33ea505af5fa304e78840505890b6420
Majid <majid@ready.net>
2024-03-10 12:03:50 -0400

    added docs

--- test/mapbox-pmtiles.test.ts
+++ test/mapbox-pmtiles.test.ts
@@ -1,6 +1,6 @@
 // sum.test.js
 import { expect, test } from 'vitest'
-import { PmTilesSource } from '../src/index'
+import PmTilesSource from '../src/index'
 
 
 const OSM_PMTILE="https://r2-public.protomaps.com/protomaps-sample-datasets/protomaps-basemap-opensource-20230408.pmtiles"
